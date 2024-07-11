cb9564c82ff439eb62c2cfc8a19b799605d5991f
Abhishek <abhishek.21b0131189@abes.ac.in>
2024-07-11 12:01:26 +0530

    Create-Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+//now understanding about the routes
 
- 
-export default function App()
-{
-  return <h1 className='text-red-500'>APP</h1>
+//so importing the some important routes
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+
+// everything now cover under the browser route-->
+//ab browser route h now unke andar routes and then sepcify each routes
+
+// Now one thing i Learn as a shortcut--> like mane home likha ushe import krna h toh m
+//generally home then space-->
+
+//Alt-shift-downarrow to cpy fast
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
