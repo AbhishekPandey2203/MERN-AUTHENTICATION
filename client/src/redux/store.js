04abba9b22e7f6b7474a65e771ccd27daccf9919
Abhishek <abhishek.21b0131189@abes.ac.in>
2024-07-13 00:39:24 +0530

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,34 @@
 // understand about the functionality of redux toolkit->
 //Generally redux we use at frontend site
 
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+
+import { persistReducer, persistStore } from "redux-persist";
+
+//the storage package help to save our data on localstorage of browser
+
+import storage from "redux-persist/lib/storage";
+
+// using of redux-persist : redux-persist
+// is a package help to keep the user login even if the user referesh the page after signin
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root", //name of the data save inside local storage
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
